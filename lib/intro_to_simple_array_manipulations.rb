0aae01f2d8954677d7f4e61dd580e4882400b01d
Nicholas Baccete <nickbaccete@gmail.com>
2020-08-10 20:48:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,4 +7,20 @@ def using_insert(array, element)
   array.insert(4, element)
 end
 
+def using_uniq(array)
+  array.uniq
+end
 
+
+def using_flatten(array)
+  array.flatten
+end
+
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def delete_at(array, integer)
+  array.delete_at[integer]
+end
